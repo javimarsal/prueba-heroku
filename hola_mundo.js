da8439ea782978bc509ab0f78007dbc665a8742d
jmaus67 <javierms5@hotmail.com>
2022-07-05 13:53:39 +0200

    app.set port

--- hola_mundo.js
+++ hola_mundo.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.set('port', process.env.PORT || port);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+//   return res.sendFile(path.join(__dirname + './index.html'));
 });
 
 app.listen(port);
